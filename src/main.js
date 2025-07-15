3ecc3bb7543f8ddb9a7ca0829d3bb3adcd92a348
admin <admin@rainycrying.com>
2025-07-15 10:34:14 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // 引入 Naive UI 插件
 import naive from 'naive-ui'
+// ✅ 引入路由实例
+import router from './router'
 
 createApp(App)
     .use(naive) // ✅ 注册 Naive UI
+    .use(router)   // ✅ 注册路由
     .mount('#app')
